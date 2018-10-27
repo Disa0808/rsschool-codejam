6afc50423f46c0d126c02cdbf10ae3681975aad0
Disa0808 <levshukdenis@mail.ru>
2018-10-28 01:53:43 +0300

    test: Add tests for 3 functions and use eslint airbnb

--- src/make.js
+++ src/make.js
@@ -1,4 +1,5 @@
-const make = function (...args) {
+const make = function make(...args) {
+    if (typeof args[0] === "function") return "Please add some numbers and then add type operation";
     const convertToSimpleArray = (array) => {
         let res = [];
         for (let i = 0; i < array.length; i++) {
@@ -14,7 +15,7 @@ const make = function (...args) {
     let store = convertToSimpleArray(args);
     function f(...arg) {
         if (typeof arg[0] === "function") {
-            return store.reduce((res, el) => arg[0](res, el), 0);
+            return store.length === 0 ? "Please add some numbers and then add type operation" : store.slice(1).reduce((res, el) => arg[0](res, el), store[0]);
         }
         store = store.concat(convertToSimpleArray([...arg]));
         return f;
